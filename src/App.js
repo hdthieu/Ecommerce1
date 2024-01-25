fe004defffc6efa3f88cdbcd5da3d8232a00275c
TriHieu <hoductrihieu@gmail.com>
2024-01-25 22:30:17 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import "./App.css";
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slides/counterSlice.js";
+import styled from "styled-components";
 function App() {
-  return <div>Hello World</div>;
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  const Button = styled.button`
+    background: grey;
+  `;
+  return (
+    <div>
+      <div>
+        <Button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </Button>
+        <span>{count}</span>
+        <Button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
